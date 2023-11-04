4cf7fad2c42b141ffc671a6a160d5f0fa7415df9
Lewis Nanson <148724417+LewisNanson@users.noreply.github.com>
2023-11-04 05:45:36 -0400

    Work on Board class, Cell class integration

--- board.py
+++ board.py
@@ -1,20 +1,27 @@
+from cell import Cell
+
 """This class is a WIP. It will be finished by Milestone 3"""
+
+
+# FIXME: decide whether we're using column/row numbers or indexes. Change __init__ and select accordingly
+# FIXME: clear & reset_to_original functions need copy of the original board. We need to figure out where to store it
 class Board:
     def __init__(self, width, height, screen, difficulty):
         """Create the board with given dimensions, window, and difficulty setting"""
+
+        # create the board
         self.cells = []
         for x in range(width):
             column = []
             for y in range(height):
-                column.append(0)  # FIXME: change for instances of Nick's Cell class
+                column.append(Cell(0, x + 1, y + 1, screen))
             self.cells.append(column)
-        self.selected_row_index = 0
-        self.selected_column_index = 0
+
+        self.selected_cell = None  # the cell the player has currently selected
 
     def select(self, row, col):
         """Select a cell at the given row and column"""
         self.selected_cell = self.cells[row - 1][col - 1]
-        # self.selected_cell.is_selected FIXME: Nick, please include a variable in your Cell class that marks whether the instance is selected
         # FIXME: handle out of bounds values
 
     def find_empty(self):
@@ -23,17 +30,19 @@ class Board:
 
     def sketch(self, value):
         """Sets sketched value of selected cell"""
-        self.selected_cell = 0  # FIXME: complete with Nick's Cell class
+        self.selected_cell.sketched_value = value
         raise NotImplementedError
 
     def place_number(self, value):
         """Sets value of selected cell"""
-        self.selected_cell = 0  # FIXME: complete with Nick's Cell class
+        self.selected_cell.value = value
         raise NotImplementedError
 
     def clear(self):
-        """Not actually sure what this function is meant to do"""
-        raise NotImplementedError
+        """Clear the selected cell"""
+        if True:  # FIXME: I think this should only work for player-selected values. Find a way to check
+            self.selected_cell.value = 0
+            self.selected_cell.sketched_value = 0
 
     def draw(self):
         """Draw the board"""
@@ -44,8 +53,12 @@ class Board:
         raise NotImplementedError("GUI not implemented")
 
     def is_full(self):
-        """Not actually sure what this function is meant to do"""
-        raise NotImplementedError
+        """Check if the board has any empty cells left"""
+        for column in self.cells:
+            for cell in column:
+                if cell.value == 0:
+                    return False
+        return True
 
     def update_board(self):
         """Not actually sure what this function is meant to do"""
