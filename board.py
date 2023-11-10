c6e31bb156f721333638d4c584416e38b70a0491
Lewis Nanson <148724417+LewisNanson@users.noreply.github.com>
2023-11-10 11:41:30 -0500

    Update board.py

--- board.py
+++ board.py
@@ -17,12 +17,21 @@ class Board:
                 column.append(Cell(0, x + 1, y + 1, screen))
             self.cells.append(column)
 
+        # save the dimensions for future reference
+        self.width = width
+        self.height = height
+
         self.selected_cell = None  # the cell the player has currently selected
 
     def select(self, row, col):
         """Select a cell at the given row and column"""
-        self.selected_cell = self.cells[row - 1][col - 1]
-        # FIXME: handle out of bounds values
+        '''the actual indexes are 1 less than the row and column numbers'''
+        row -= 1
+        col -= 1
+        try:
+            self.selected_cell = self.cells[col][row]
+        except IndexError:
+            print(f'There is no cell at ({col}, {row}). There are {self.width} columns and {self.height} rows.')
 
     def find_empty(self):
         """Returns tuple with row and column of an empty cell"""
@@ -30,23 +39,25 @@ class Board:
 
     def sketch(self, value):
         """Sets sketched value of selected cell"""
-        self.selected_cell.sketched_value = value
-        raise NotImplementedError
+        self.selected_cell.set_sketched_value(value)
 
     def place_number(self, value):
         """Sets value of selected cell"""
-        self.selected_cell.value = value
-        raise NotImplementedError
+        self.selected_cell.set_cell_value(value)
 
     def clear(self):
-        """Clear the selected cell"""
-        if True:  # FIXME: I think this should only work for player-selected values. Find a way to check
-            self.selected_cell.value = 0
-            self.selected_cell.sketched_value = 0
+        """Clear the selected cell by setting both its value and sketched value back to 0"""
+        if True:  # FIXME: I think this should only work for player-set values. Find a way to check
+            self.selected_cell.set_cell_value(0)
+            self.selected_cell.set_sketched_value(0)
 
     def draw(self):
         """Draw the board"""
-        raise NotImplementedError("GUI not implemented")
+        # FIXME: placeholder behavior
+        for y in range(self.height):
+            for x in range(self.width):
+                print(self.cells[x][y].value, end=' ')
+            print()
 
     def click(self, x, y):
         """Handle mouse clicks at a point on the board"""
@@ -56,9 +67,9 @@ class Board:
         """Check if the board has any empty cells left"""
         for column in self.cells:
             for cell in column:
-                if cell.value == 0:
+                if cell.value == 0:  # cell is empty
                     return False
-        return True
+        return True  # no empty cell found
 
     def update_board(self):
         """Not actually sure what this function is meant to do"""
@@ -71,3 +82,42 @@ class Board:
     def check_board(self):
         """Check if the board has been solved"""
         raise NotImplementedError  # FIXME: Nick will implement this function
+
+
+'''Everything below this point is testing code that will only run if board.py is launched directly'''
+if __name__ == "__main__":
+    choices = [
+        'Select',  # 1
+        # 'Find empty',
+        'Sketch',  # 2
+        'Place number',  # 3
+        'clear',  # 4
+        'draw',  # 5
+        # 'click',
+        'Is full',  # 6
+        # 'Update board',
+        # 'Reset to original',
+        # 'Check board'
+    ]
+
+    test_board = Board(9, 9, None, 0)
+    choice = 0
+    while choice != -1:
+        for (index, option) in enumerate(choices):
+            print(f"{index + 1}. " + option)
+
+        choice = int(input())
+        if choice == 1:
+            row = int(input("Row:"))
+            col = int(input("Col:"))
+            test_board.select(row, col)
+        elif choice == 2:
+            test_board.sketch(int(input("Value:")))
+        elif choice == 3:
+            test_board.place_number(int(input("Value:")))
+        elif choice == 4:
+            test_board.clear()
+        elif choice == 5:
+            test_board.draw()
+        elif choice == 6:
+            print(test_board.is_full())
