e29950682c76b9accad372a51641f21bd959472c
pecacho214 <nickcampos06@gmail.com>
2023-11-19 00:54:01 -0500

    added main menu GUI

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
 import pygame
+import main_menu
 from sudoku_generator import SudokuGenerator
 from board import Board
 from cell import Cell
 from constants import *
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Sudoku")
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Sudoku")
 
-screen.fill(BACKGROUND_COLOR)
+    screen.fill(BACKGROUND_COLOR)
 
-display_main_menu = True
-display_game = False
-display_win = False
+    display_main_menu = True
+    display_game = False
+    display_win = False
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-        if display_main_menu:
-            pass
-            # this should call on a function that displays the main menu
-            # the function should return a game difficulty and should update display_game to True
-            # feel free to change this logic if it doesn't work right
-        if display_game:
-            pass
-            # this should call on a function that displays the game
-
-
-
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if display_main_menu:
+                main_menu.displayMainMenu(screen, WIDTH, HEIGHT)
+                # this should call on a function that displays the main menu
+                # the function should return a game difficulty and should update display_game to True
+                # feel free to change this logic if it doesn't work right
+            if display_game:
+                pass
+                # this should call on a function that displays the game
+        pygame.display.update()
 
 
-    pygame.display.update()
+if __name__ == "__main__":
+    main()
