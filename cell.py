ad01c63c29ee5de6e7f3bd89bcce435a6d829f6b
Lewis Nanson <148724417+LewisNanson@users.noreply.github.com>
2023-11-23 07:27:28 -0500

    Board rendering

--- cell.py
+++ cell.py
@@ -1,17 +1,19 @@
 import pygame
 from constants import *
 
+
 class Cell:
+
     def __init__(self, value, row, col, screen):
         self.value = value
         self.row = row
         self.col = col
         self.screen = screen
         self.sketched_value = 0
-        # self.cell_font = pygame.font.Font(None, VALUE_FONT_SIZE)
+        self.cell_font = pygame.font.Font(None, VALUE_FONT_SIZE)
         # initialise variables
 
-    def set_cell_value(self, value): # pass this function sketched value
+    def set_cell_value(self, value):  # pass this function sketched value
         self.value = value
         return self.value
         # method that sets the value of the cell
@@ -22,37 +24,43 @@ class Cell:
         try:
             if value >= 1 and value <= 9:
                 self.sketched_value = value
-            else: 
+            else:
                 raise ValueError
         except ValueError:
             self.sketched_value = 0
         finally:
             return self.sketched_value
-        
+
         # method that sets the sketched value of the cell
         # should be called when user hits a number
         # sets the sketched value to 0 if the user input is invalid in any way
         # 0 is placeholder for no sketched value
-    
-    def draw_sketched_value(self): # pass this function sketched value
+
+    def draw_sketched_value(self):  # pass this function sketched value
         if self.sketched_value == 0:
             cell_num_surface = self.cell_font.render(None, 0, SELECTED_CELL_COLOR)
         else:
             cell_num_surface = self.cell_font.render(str(self.sketched_value), 0, SELECTED_CELL_COLOR)
-        cell_num_rect = cell_num_surface.get_rect(center=(CELL_SIZE * self.col + CELL_SIZE / 2 + 22.5, CELL_SIZE * self.row + CELL_SIZE / 2  + 22.5))
+        cell_num_rect = cell_num_surface.get_rect(
+            center=(CELL_SIZE * self.col + CELL_SIZE / 2 + 22.5, CELL_SIZE * self.row + CELL_SIZE / 2 + 22.5))
         self.screen.blit(cell_num_surface, cell_num_rect)
 
     def draw_final_value(self):  # when user hits enter, this function should be called
-        if self.value == 0:
-            cell_num_surface = self.cell_font.render(None, 0, FONT_COLOR)
-        else:
+        if self.value != 0:
+            start_x, end_x = LEFT_MARGIN, self.screen.get_width() - RIGHT_MARGIN
+            start_y, end_y = TOP_MARGIN, self.screen.get_height() - BOTTOM_MARGIN
+            range_x, range_y = end_x - start_x, end_y - start_y
+
+            x = (range_x / 9) * (self.col + 0.5) + start_x
+            y = (range_y / 9) * (self.row + 0.5) + start_y
+
             cell_num_surface = self.cell_font.render(str(self.value), 0, FONT_COLOR)
-        cell_num_rect = cell_num_surface.get_rect(center=(CELL_SIZE * self.col + CELL_SIZE / 2 + 22.5, CELL_SIZE * self.row + CELL_SIZE / 2 + 22.5))
-        self.screen.blit(cell_num_surface, cell_num_rect)
+            cell_num_rect = cell_num_surface.get_rect(center=(x, y))
+            self.screen.blit(cell_num_surface, cell_num_rect)
 
         # method draws cell on the screen
         # this is where pygame is needed
 
     def draw(self):
-        self.draw_sketched_value()
+        # self.draw_sketched_value()
         self.draw_final_value()
