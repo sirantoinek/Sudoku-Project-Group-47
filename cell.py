298fa4b944f0b0290a30afac41506671310c14ff
Lewis Nanson <148724417+LewisNanson@users.noreply.github.com>
2023-11-23 13:09:35 -0500

    Fixes and sketched value rendering

--- cell.py
+++ cell.py
@@ -10,7 +10,8 @@ class Cell:
         self.col = col
         self.screen = screen
         self.sketched_value = 0
-        self.cell_font = pygame.font.Font(None, VALUE_FONT_SIZE)
+        self.value_font = pygame.font.Font(None, VALUE_FONT_SIZE)
+        self.sketched_font = pygame.font.Font(None, SKETCHED_FONT_SIZE)
         # initialise variables
 
     def set_cell_value(self, value):  # pass this function sketched value
@@ -37,13 +38,17 @@ class Cell:
         # 0 is placeholder for no sketched value
 
     def draw_sketched_value(self):  # pass this function sketched value
-        if self.sketched_value == 0:
-            cell_num_surface = self.cell_font.render(None, 0, SELECTED_CELL_COLOR)
-        else:
-            cell_num_surface = self.cell_font.render(str(self.sketched_value), 0, SELECTED_CELL_COLOR)
-        cell_num_rect = cell_num_surface.get_rect(
-            center=(CELL_SIZE * self.col + CELL_SIZE / 2 + 22.5, CELL_SIZE * self.row + CELL_SIZE / 2 + 22.5))
-        self.screen.blit(cell_num_surface, cell_num_rect)
+        if self.sketched_value != 0:
+            start_x, end_x = LEFT_MARGIN, self.screen.get_width() - RIGHT_MARGIN
+            start_y, end_y = TOP_MARGIN, self.screen.get_height() - BOTTOM_MARGIN
+            range_x, range_y = end_x - start_x, end_y - start_y
+
+            x = (range_x / 9) * (self.col + 0.2) + start_x
+            y = (range_y / 9) * (self.row + 0.3) + start_y
+
+            cell_num_surface = self.sketched_font.render(str(self.value), 0, SECONDARY_FONT_COLOR)
+            cell_num_rect = cell_num_surface.get_rect(center=(x, y))
+            self.screen.blit(cell_num_surface, cell_num_rect)
 
     def draw_final_value(self):  # when user hits enter, this function should be called
         if self.value != 0:
@@ -54,7 +59,7 @@ class Cell:
             x = (range_x / 9) * (self.col + 0.5) + start_x
             y = (range_y / 9) * (self.row + 0.5) + start_y
 
-            cell_num_surface = self.cell_font.render(str(self.value), 0, FONT_COLOR)
+            cell_num_surface = self.value_font.render(str(self.value), 0, FONT_COLOR)
             cell_num_rect = cell_num_surface.get_rect(center=(x, y))
             self.screen.blit(cell_num_surface, cell_num_rect)
 
@@ -62,5 +67,5 @@ class Cell:
         # this is where pygame is needed
 
     def draw(self):
-        # self.draw_sketched_value()
+        self.draw_sketched_value()
         self.draw_final_value()
